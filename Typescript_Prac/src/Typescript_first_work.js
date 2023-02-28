06628646e056c34d74b55ff6d498effa02bdfd5a
BaekJinH <tmdfyd6868@naver.com>
2023-02-28 11:02:59 +0900

    .

---
+++ Typescript_Prac/src/Typescript_first_work.js
@@ -0,0 +1,25 @@
+"use strict";
+var str = 'hi';
+var num = 100;
+var arr2 = [1, 2, 3];
+var obj = {};
+var obj2 = {
+    name: 'hoho',
+    age: 22
+};
+function add(a, b) {
+    return a + b;
+}
+function aa() {
+}
+function dd() {
+}
+var a;
+var b;
+var c;
+var d;
+var f;
+var e;
+function test() {
+    var g;
+}
---
+++ Typescript_Prac/src/Typescript_first_work.ts
@@ -0,0 +1,31 @@
+let str: string = 'hi';
+let num: number = 100;
+
+let arr2: number[] = [1, 2, 3];
+
+let obj: object = {};
+let obj2: { name: string, age: number} = {
+    name: 'hoho',
+    age: 22
+};
+
+function add(a: number, b: number): number {
+    return a+b;
+    }
+
+function aa() {
+
+}
+function dd () {
+
+}
+let a;
+let b;
+let c;
+let d;
+let f;
+let e;
+function test() {
+    let g;
+    let t;
+}
