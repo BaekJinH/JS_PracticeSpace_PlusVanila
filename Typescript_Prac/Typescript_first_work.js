888cd87932ff2538a2d40cdb04b341a27dfbbe1a
BaekJinH <tmdfyd6868@naver.com>
2023-02-28 10:02:51 +0900

    .

--- Typescript_Prac/Typescript_first_work.js
+++ Typescript_Prac/Typescript_first_work.js
@@ -1,2 +1,11 @@
-var _a;
-(_a = document.querySelector('.test')) === null || _a === void 0 ? void 0 : _a.addEventListener;
+var str = 'hi';
+var num = 100;
+var arr2 = [1, 2, 3];
+var obj = {};
+var obj2 = {
+    name: 'hoho',
+    age: 22
+};
+function add(a, b) {
+    return a + b;
+}
--- Typescript_Prac/Typescript_first_work.ts
+++ Typescript_Prac/Typescript_first_work.ts
@@ -1,1 +1,14 @@
-document.querySelector('.test')?.addEventListener
+let str: string = 'hi';
+let num: number = 100;
+
+let arr2: number[] = [1, 2, 3];
+
+let obj: object = {};
+let obj2: { name: string, age: number} = {
+    name: 'hoho',
+    age: 22
+};
+
+function add(a: number, b: number): number {
+    return a+b;
+    }
