34978f1ee8275d6d800bf746f5001f0d1cffb74b
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-05-22 17:57:10 +0900

    .

--- workSpace_homepage/resources/js/effect.js
+++ workSpace_homepage/resources/js/effect.js
@@ -48,18 +48,23 @@ window.addEventListener('load' , () => {
 
 
 // 햄버거 클래스 토글
-function hambAnimate () {
-    document.querySelector('.hambuger').classList.toggle('act_hamb')
-}
+document.querySelector('.hambuger').addEventListener('click' , () => {
+    let hamb = document.querySelector('.hambuger') ;
+    hamb.classList.toggle('act_hamb') ;
+    hamb.classList.contains('act_hamb') ? hamb.classList.remove('return') : hamb.classList.add('return')
+})
 
 
 // 하단 카테고리 박스 이벤트 + 이펙트 / 돔 컨텐츠 로드 후 2초 지나면 하단 카테고리 생성 후 다시 숨기기
 function box_eventTime () {
     document.addEventListener('DOMContentLoaded' , e => {
         window.setTimeout( () => {
-            document.querySelector('.category_box').classList.add('move_box')
+            document.querySelector('.category_box').classList.add('fixed_foot')
             window.setTimeout( () => {
-                document.querySelector('.category_box').classList.remove('move_box')
+                document.querySelector('.category_box').classList.remove('fixed_foot')
+                if ( document.querySelector('header').classList.contains('fixed_header') ) {
+                    document.querySelector('.category_box').classList.add('fixed_foot')
+                }
             }, 5000)
         }, 2000)
     })
@@ -91,18 +96,18 @@ function bonuce_animate () {
 //     });
 // }
 
+
 // openEffect()
-
-
 function openEffect () {
     const moveBox = document.querySelector('.category_box');
     moveBox.addEventListener('mouseover' , e => {
         console.log('a')
-        moveBox.classList.add('open_box')
+        document.querySelector('.category_box').classList.add('open_box')
     })
     moveBox.addEventListener('mouseleave' , e => {
         console.log('b')
-        moveBox.classList.remove('open_box')
+        document.querySelector('.category_box').classList.remove('open_box')
+
     })
 }
 
@@ -116,7 +121,7 @@ document.addEventListener('scroll' , e => {
     let now_scroll = window.scrollY
 
     // 하단 카테고리 박스가 어느 위치에서 나올지 위치 값
-    const box_fixedHeight = document.querySelector("header").offsetHeight + 600;
+    // const box_fixedHeight = document.querySelector("header").offsetHeight + 600;
 
     // 헤더의 높이 값
     let pin_height = document.querySelector("header").offsetHeight;
@@ -126,16 +131,19 @@ document.addEventListener('scroll' , e => {
 
     pin_result > now_scroll ? header_removeClass() : header_addClass()
 
+
     function header_addClass () {
         document.querySelector('header').classList.add('fixed_header')
+        document.querySelector('.category_box').classList.add('fixed_foot')
     }
     function header_removeClass () {
         document.querySelector('header').classList.remove('fixed_header')
+        document.querySelector('.category_box').classList.remove('fixed_foot')
     }
 
-    console.log(pin_result)
-    console.log(box_fixedHeight)
+    // console.log(box_fixedHeight)
 })
+
 
 // 키보드에서 글자 입력하면 absolute로 플로팅 처리해서 위로 띄우기 -> 해당 코드 완성되면 입력했던 키가 엔터키를 누르기 전까지는 배열로 들어가서 합쳐지도록 만들기 될란지는;
 let floatBtn = document.querySelector('.float_effect_on') ;
@@ -174,19 +182,63 @@ function textFloat () {
 // console.log(floatBtn) -- 확인용
 
 
+
+// DOM이라 오류 남
+// ptCircle.style.width = ptSize ;
+// ptCircle.style.height = ptSize ;
+// ptCircle.style.backgroundColor = `rgba(${ptColor} + ${ptColor} + ${ptColor} + 0.7)` ;
 // 홈페이지 효과용 Canvas
+
 const ptCanvas = document.querySelector('#canvas')
-const ctx = ptCanvas.getContext('2d')
 ptCanvas.width = window.innerWidth;
 ptCanvas.height = window.innerHeight;
-let isClick = false;
+const ctx = ptCanvas.getContext('2d')
 ptCanvas.addEventListener('click' , particleEffect)
 
-function particleEffect () {
+function particleEffect (e) {
+    let pointX = e.clientX;
+    let pointY = e.clientY;
+    let ptCount = Math.floor(Math.random() * 20) ;
+    let rndNum ;
+    let createPosX = [pointX]
+    let createPosY = [pointY]
+    // Start Drawing Circle
+    for ( let i = 1 ; i < ptCount ; i++) {
+        rndNum = Math.random()
+        pointX += Math.floor(Math.random() * 50 )
+        pointY += Math.floor(Math.random() * 50 )
 
+        if ( rndNum <= 0.5 ) {
+            pointX *= -1
+            pointY *= -1
+        }
+        else {
+            pointX
+            pointY
+
+        }
+        createPosX.push(pointX);
+        createPosY.push(pointY);
+        let ptSize = Math.floor(Math.random() * 30);
+        let ptColor = [ Math.floor(Math.random() * 255) , Math.floor(Math.random() * 255) , Math.floor(Math.random() * 255) ] ;
+        ctx.beginPath()
+        ctx.arc(pointX, pointY, ptSize, 0, Math.PI * 2, false) ;
+        ctx.fillStyle = `rgb(${ptColor[0]} , ${ptColor[1]} , ${ptColor[2]})`;
+        ctx.fill()
+        ctx.closePath()
+    }
+
+
+    console.log(createPosX , createPosY , rndNum , pointX )
+    // console.log(ptSize , ptCount , pointX , pointY , ctx )
 }
 
 
+// ctx.ptCircle
+// for ( let e = 0 ; e < ptColor.length ; e++) {
+//     console.log(e)
+//     ctx.fillStyle = `rgb(${ptColor} , ${ptColor} , ${ptColor}  )`;
+// }
 // Game Canvas 불러오기
 // 확인용
 let gameInstance = new game()
@@ -208,9 +260,38 @@ console.log(loadEffect.textScreen())
 
 
 
-// Modal 창 생성 후 오픈
-let mainMd = document.querySelector('.modal') ;
-console.log(mainMd)
+// Modal 창 생성 후 오픈 / 목표 -> DOM의 mainMd 안의 컨텐츠를 가져온 후 그걸 그대로 모달로 생성. 모달을 닫으면 컨텐츠 비우기
+
+// // Object.assign 방식
+// function openNew_modal () {
+//     let mainMd = document.querySelector('.modal_base') ;
+//     document.body.appendChild(
+//         Object.assign(
+//             document.createElement('div'),
+//             { id : 'modal'} ,
+//             { textContent : 'hello'}
+//         )
+//     )
+//     console.log(mainMd)
+
+// }
+// // modal 생성 테스트용 다 만들어지면 onclick으로 뺴버리기
+
+
+let mainMd = document.querySelector('.modal_base') ;
+let baseContent = mainMd.innerHTML ;
+
+let modalWrap = document.querySelector('#modal_area') ;
+let modalIn = document.querySelector('#modal_area .modal_inner') ;
+document.querySelector('.open_modal').addEventListener('click' , () => {
+    modalIn.innerHTML = `
+        ${baseContent}
+    ` ;
+
+    modalWrap.style.zIndex = 1
+    // document.body.style.overflow = hidden;
+    console.log(modalWrap)
+})
 
 
 
@@ -218,16 +299,16 @@ console.log(mainMd)
 
 
 // 인풋 창 내용 입력 시 모달 창 생성 후 플롯 => 보류
-let mdContents = document.querySelector('.modal_int')
-let otherContents = document.querySelector('input[type=text]')
+// let mdContents = document.querySelector('.modal_int')
+// let otherContents = document.querySelector('input[type=text]')
 
-let mdSet = document.createElement( 'div' )
-mdSet.innerHTML = `
-    <div class="modalInner">
-        <div class="close"></div>
-        <div class="main">
-            <div class="contents"> ${otherContents} </div>
-        </div>
-    </div>
-    `
-console.log(mdSet)
+// let mdSet = document.createElement( 'div' )
+// mdSet.innerHTML = `
+//     <div class="modalInner">
+//         <div class="close"></div>
+//         <div class="main">
+//             <div class="contents"> ${otherContents} </div>
+//         </div>
+//     </div>
+//     `
+// console.log(mdSet)
