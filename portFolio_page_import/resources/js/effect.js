89b1ddbc65a36c3ac32eb96435233f9a572b383d
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-07-03 17:21:27 +0900

    .

---
+++ portFolio_page_import/resources/js/effect.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    // 햄버거
+    let hambMenu = document.querySelector('.hambuger')
+    if ( hambMenu ) {
+        hambMenu.addEventListener('click' , () => {
+            hambMenu.classList.add('hambOn')
+        })
+    }
+
+
+    //  서브 서클
+    let circle = document.querySelector('.subCircle')
+    if ( circle ) {
+        circle.addEventListener('click' , (e) => {
+
+        })
+    }
+
+
+    // 스크롤
+    window.addEventListener('scroll' , function (e) {
+        let currTop = window.scrollY
+        // console.log(currTop)
+        // if ( currTop >= otherTop ) {
+
+        // }
+    })
+
+
+    // 풀페이지 전환 버튼
+    let changeBtn = document.querySelector('.screenChange') ;
+
+
+    // 마우스 휠 이벤트
+    window.addEventListener('mousewheel' , e => {
+        if (e.deltaY > 0) {
+            let scroll = window.innerHeight;
+            window.scrollTo({
+                // top: scroll,
+                behavior: "smooth" ,
+            });
+            console.log(scroll);
+        }
+        else {
+
+        }
+    })
+
+
+    // 풀페이지
+    const contentLeng = Array.from(document.querySelectorAll('.fullContainer > .content'));
+    contentLeng.forEach(el => {
+        let dot = document.createElement('div') ;
+        let scroll = window.scrollY ;
+        dot.className = 'dot' ;
+        document.querySelector('.dotCont').appendChild(dot);
+
+        dot.addEventListener('click' , function (e) {
+
+        })
+    });
+
+
+
+    // 테마 색 반전
+
+
+
+})
---
+++ portFolio_page_import/resources/js/intro.js
@@ -0,0 +1,20 @@
+// window.addEventListener('load' , () => {
+//     let loadingScreen = document.querySelector('.load') ;
+//     if (loadingScreen) {
+//         alert('Loading screen exists!');
+//         loadingScreen.remove();
+//     } else {
+//         alert('Loading screen does not exist.');
+//     }
+// })
+
+// (function createLoadingScreen() {
+//     let loadingScreen = document.createElement('div');
+//     loadingScreen.className = 'load';
+
+//     loadingScreen.innerHTML = `
+//         <div class='introText'>감사합니다.</div>
+//     `;
+
+//     document.body.appendChild(loadingScreen);
+// })();
