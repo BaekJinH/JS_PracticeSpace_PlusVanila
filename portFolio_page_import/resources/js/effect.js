67eb912a5a34e436b9e22d101d26f501d49bd5c4
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-07-06 17:59:20 +0900

    .

--- portFolio_page_import/resources/js/effect.js
+++ portFolio_page_import/resources/js/effect.js
@@ -1,15 +1,114 @@
 document.addEventListener('DOMContentLoaded' , () => {
     // 햄버거
-    let hambMenu = document.querySelector('.hambuger') ;
-    if ( hambMenu ) {
-        let blackPage = document.querySelector('.blackPager') ;
-        hambMenu.addEventListener('click' , e => {
-            hambMenu.classList.toggle('act_hamb') ;
-            hambMenu.classList.contains('act_hamb') ? hambMenu.classList.remove('return') : hambMenu.classList.add('return')
+    let hambMenu = document.querySelector('.hambuger');
+    let blackPage = document.querySelector('.blackPager');
+    let animating = false;
+    let sdTxt = Array.from(document.querySelectorAll('.blackPager li'));
 
-            console.log(blackPage)
-        })
+    // animeJs
+    let screenClose = anime({
+        targets: blackPage,
+        autoplay: false,
+        left: ['100%', '0%'],
+        easing: 'easeOutElastic(6, .4)',
+        duration: 4000,
+        complete: function() { // 애니메이션이 끝나면 animating을 false로 변경
+            animating = false;
+        }
+    });
+    let screenOpen = anime({
+        targets: blackPage,
+        autoplay: false,
+        left: ['0%', '100%'],
+        easing: 'easeInOutBack',
+        duration: 1000,
+        begin: function() { // 애니메이션이 시작하면 animating을 true로 변경
+            animating = true;
+        },
+        complete: function() { // 애니메이션이 끝나면 animating을 false로 변경
+            animating = false;
+        }
+    });
+
+
+    if (hambMenu) {
+        blackPage.style.left = `100%`;
+        hambMenu.addEventListener('click', async e => {
+            if (animating) return;
+            animating = true;
+            hambMenu.classList.toggle('act_hamb');
+            hambMenu.classList.contains('act_hamb') ?
+            (
+                hambMenu.classList.remove('default'),
+                hambMenu.classList.remove('return'),
+                bpOn(),
+                (screenClose.paused ? screenClose.play() : null),
+                screenClose.restart(),
+                await new Promise(resolve => setTimeout(resolve, 4000)), // wait for 4 seconds
+                (async () => {
+                    for (const element of sdTxt) {
+                        await new Promise(resolve => setTimeout(() => {
+                            element.classList.remove('slidePass');
+                            element.classList.add('slideText');
+                            let txtSd = anime({
+                                targets: element,
+                                duration: 1000,
+                                autoplay: true,
+                                easing: 'easeInOutQuad',
+                                translateX: ['-100%','0%'],
+                                begin: function() {
+                                    animating = true;
+                                },
+                                complete: function() {
+                                    animating = false;
+                                }
+                            });
+                            resolve();
+                        }, sdTxt.indexOf(element) * 100));
+                    }
+                })()
+            ) :
+            (
+                hambMenu.classList.add('return'),
+                bpOff(),
+                await new Promise(resolve => setTimeout(resolve, 500)),
+                (async () => {
+                    await sdTxt.reduce(async (prevPromise, e, i) => {
+                        await prevPromise;
+                        await new Promise(resolve => setTimeout(() => {
+                            e.classList.remove('slideText');
+                            e.classList.add('slidePass');
+                            let txtPs = anime({
+                                targets: e,
+                                duration: 500,
+                                autoplay: true,
+                                easing: 'easeInOutQuad',
+                                translateX: ['0%', '100%'],
+                            });
+                            resolve();
+                        }, i * 100));
+                    }, Promise.resolve());
+                    (screenOpen.paused ? screenOpen.play() : null),
+                    screenOpen.restart()
+                })()
+            );
+        });
     }
+
+    // black Page
+    let bpOn = function () {
+        blackPage.classList.remove('bpReturn');
+        blackPage.classList.add('bpOn');
+    }
+
+    let bpOff = function () {
+        blackPage.classList.remove('bpOn');
+        blackPage.classList.add('bpReturn');
+    }
+
+
+
+
 
 
     // Canvas 백그라운드 코드를 이해하고 작성할 수 있게 되면 사용하기
