e0985a9de0a4ee340d540b7e3d14bc4bed332163
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2023-09-07 14:45:49 +0900

    .

--- React_Prac/blog/src/Header.js
+++ React_Prac/blog/src/Header.js
@@ -1,8 +1,8 @@
 import React, { useRef, useState} from 'react';
 import logo from './image/logo.png' ;
 import btnList from './data/searchData.json' ;
+import bells from './data/bell.json' ;
 import './css/App.css';
-import './css/reset.css';
 
 function Header() {
     const [active, setActive] = useState(false);
@@ -17,28 +17,40 @@ function Header() {
             }
         }
     }
-
     const divClasses = ['top' , 'mid' , 'bot'] ;
 
     return (
         <div id='header'>
             <header className="App-header" >
                 <ul className='topAside'>
-                    <li className={active ? `active hamb` : `hamb`} ref={liRef} onClick={hambHandle}>
-                        {divClasses.map(cls => <div key={cls} className={cls}></div>)}
-                    </li>
-                    <li className='pay'>
-                        <a href=""></a>
-                    </li>
+                    <ul className='left'>
+                        <li className={active ? `active hamb` : `hamb`} ref={liRef} onClick={hambHandle}>
+                            {divClasses.map(cls => 
+                                <div key={cls} className={cls}></div>
+                            )}
+                        </li>
+                        <li className='pay'>
+                            <a href="#none">Naver Pay</a>
+                        </li>
+                    </ul>
+                    <ul className='right'>
+                        <li className="chat"></li>
+                        <li className="bell">
+                            {bells.length > 0 && (
+                                <span className='notification=count'>{bells.length}</span>
+                            )}
+                        </li>
+                    </ul>
                 </ul>
                 <div className="search">
                     <img src={logo} className="App-logo" alt="logo" />
                     <form action="" id='searchForm' method='get'>
                         <fieldset>
-                            <input type="text" />
+                            <input type="text" id="typingView" placeholder='검색어를 입력하세요'/>
                         </fieldset>
                     </form>
                     <button type='submit'>
+                        검색하기
                     </button>
                 </div>
                 <div className="otherBtn">
@@ -47,7 +59,6 @@ function Header() {
                             <li key={index} className={"btn_"+item.className}>
                                 <a href={item.href} rel="noopener noreferrer" target={item.isBlank}>
                                     {item.a}
-                                    
                                 </a>
                             </li>
                         ))}
