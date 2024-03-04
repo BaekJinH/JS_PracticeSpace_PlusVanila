c85278b715f8c5de566f31f6ea5a732d81879a07
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2024-03-04 17:51:24 +0900

    Update server.dev.js

--- frontend_portfolio/dist/server.dev.js
+++ frontend_portfolio/dist/server.dev.js
@@ -1,28 +1,26 @@
-"use strict";
+let express = require('express');
 
-var express = require('express');
+let app = express();
+let PORT = 4000;
 
-var app = express();
-var PORT = 4000;
-
-var cors = require('cors');
+let cors = require('cors');
 
 app.use(cors()); // CORS 문제를 해결하기 위해 모든 출처에서의 요청을 허용
 
 app.use(express.json()); // JSON 형식의 요청 본문을 파싱하기 위한 미들웨어 설정
 // Todo 항목을 저장할 배열
 
-var todos = []; // 모든 Todo 항목을 제공하는 GET 요청 처리
+let todos = []; // 모든 Todo 항목을 제공하는 GET 요청 처리
 
 app.get('/todos', function (req, res) {
   res.json(todos);
 }); // 새 Todo 항목을 생성하는 POST 요청 처리
 
 app.post('/todos', function (req, res) {
-  var title = req.body.title; // 요청 본문에서 title을 추출
+  let title = req.body.title; // 요청 본문에서 title을 추출
 
-  var todo = {
-    title: title
+  let todo = {
+    title: title,
   }; // 새 Todo 객체 생성
 
   todos.push(todo); // Todo 배열에 추가
@@ -31,5 +29,5 @@ app.post('/todos', function (req, res) {
 }); // 서버 시작
 
 app.listen(PORT, function () {
-  console.log("Server is running on http://localhost:".concat(PORT));
+  console.log('Server is running on http://localhost:'.concat(PORT));
 });
